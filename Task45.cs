5ae1f1eabeb76943a38db3f32e0e7a7b006f44ca
YaroslavDubV <yaroslavduboshin@mail.ru>
2022-08-27 22:56:15 +0300

    Task45

---
+++ Task45.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int fact(int N) {
+    if (N == 1) return 1;
+    fact(N - 1);
+    return N * fact(N - 1);
+}
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(fact(n));
